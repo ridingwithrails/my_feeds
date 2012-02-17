b5f634df32c1f123c0905efc896d38a52606a8a0
Owen Masback <omasback@gmail.com>
2012-02-17 15:25:18 -0800

    display json feeds

--- app/assets/javascripts/feeds_contoller.js
+++ app/assets/javascripts/feeds_contoller.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+	$.getJSON("/feeds.json", function(data) {
+		for (i=0; i<data.length; i++) {
+			var story = $.parseJSON(data[i]);
+			var storyContent = "<div class='story'><h2><a href='" + story.url + "'>" + story.title + "</a></h2><div class='summary'>" + story.summary + "</div></div>";
+			$("#stories").append(storyContent);
+		}
+	});
+
+});
